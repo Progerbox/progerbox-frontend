eade296248ab7c9ccd2ffd1262f7ea7ee85aa635
SiRanWeb <kirillg.web@gmail.com>
2023-03-07 22:42:40 +0300

    rename interface

--- src/widgets/layouts/main-layout/ui/sidebar/sidebar.tsx
+++ src/widgets/layouts/main-layout/ui/sidebar/sidebar.tsx
@@ -3,13 +3,13 @@ import styles from './sidebar.module.scss';
 import React, { FC } from 'react';
 import { uniteClasses } from '@/shared';
 
-interface ISidebar {
+interface Props {
   categoriesData: ISidebarSection[];
   className?: string;
 }
 
 // TODO in future - move out to shared layer and reuse there (to make different layouts with sidebar)
-export const Sidebar: FC<ISidebar> = ({ categoriesData, className }) => {
+export const Sidebar: FC<Props> = ({ categoriesData, className }) => {
   return (
     <div className={uniteClasses(styles.sidebar, className)}>
       {categoriesData.map((data, index) => (
